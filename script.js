0d9c4b43e3459a29d9708aa0c8a19ed242731e26
Nikhil Rajput <>
2021-04-18 12:34:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var config = {
+	settings:{
+        hasHeaders: true,
+        showPopoutIcon: false,
+    },
+	content: [{
+		type: 'row',
+		content: [{
+			type: 'component',
+			componentName: 'gridComponent',
+			title: 'code.cpp',
+			componentState: {
+				html: getContent('#goldenify-a'),
+			}
+		}, {
+			type: 'column',
+			content: [{
+				type: 'component',
+				componentName: 'gridComponent',
+				title: 'input.txt',
+				componentState: {
+					html: getContent('#goldenify-b')
+				}
+			}, {
+				type: 'component',
+				componentName: 'gridComponent',
+                title: 'output.txt',
+				componentState: {
+					html: getContent('#goldenify-c')
+				}
+			}]
+		}]
+	}]
+};
+
+var myLayout = new GoldenLayout(config);
+
+myLayout.registerComponent('gridComponent', function(container, componentState) {
+	container.getElement().html(componentState.html);
+});
+
+myLayout.init();
+
+function getContent(selector) {
+	var $el = $(selector);
+	var html = $el.html();
+
+	$el.remove();
+
+	return html;
+}
