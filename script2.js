800b5ba97f791a8cb6e2cd22e595bda1fd77b8c2
Nikhil Rajput <>
2021-05-23 11:16:06 +0000

    update

--- script2.js
+++ script2.js
@@ -209,8 +209,8 @@ function editorsUpdateFontSize(fontSize) {
     editor4.updateOptions({fontSize: fontSize});
 }
 
-var out_audio = new Audio('outputaudio.mp3');
-var run_audio = new Audio('runaudio.wav');
+var out_audio = new Audio('https://cdn.jsdelivr.net/gh/nikhraj/CodeForces-Code-Editor/outputaudio.mp3');
+var run_audio = new Audio('https://cdn.jsdelivr.net/gh/nikhraj/CodeForces-Code-Editor/runaudio.wav');
 //var btn_audio = new Audio('buttonaudio.mp3');
 
 $("body").keydown(function (e) {
