1c787336bac374173d622bac5ca0ae51192ac650
nikhraj <nikhil2018.rajput@gmail.com>
2021-05-24 20:28:32 +0530

    static changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
