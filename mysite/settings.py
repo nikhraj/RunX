db899bab1731a25f4fd69fe99d9f69bb8371faaf
nikhraj <nikhil2018.rajput@gmail.com>
2021-05-24 21:04:08 +0530

    settings changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
